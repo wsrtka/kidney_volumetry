eca1ebc2e7d180d9d263fc45c467367957117052
wsrtka <witold.serwatka@pm.me>
2022-08-30 21:03:25 +0200

    add function to find relevant segmentation slices

--- volumetry/utils.py
+++ volumetry/utils.py
@@ -9,3 +9,23 @@ def divide_image(image):
     left_part = arr[:,:,:half]
     right_part = arr[:,:,half:]
     return left_part, right_part
+
+def find_kidney_slices(segmentation):
+    max_idx = None
+    first_idx = segmentation.shape[2]
+    last_idx = 0
+    max_counts = 0
+
+    for idx, seg_slice in enumerate(segmentation):
+        mask = seg_slice == 1
+        _, counts = np.unique(mask, return_counts=True)
+        if len(counts) > 1:
+            if counts[1] > max_counts:
+                max_counts = counts[1]
+                max_idx = idx
+            if idx < first_idx:
+                first_idx = idx
+            if idx > max_idx:
+                last_idx = idx
+
+    return first_idx, last_idx, max_idx
